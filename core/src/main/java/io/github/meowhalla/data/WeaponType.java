4432802c13cd68df74e292e87431911532e3f4af
Hortensjaa <lilka305@gmail.com>
2025-04-16 19:11:22 +0200

    weapons' projectiles added

---
+++ core/src/main/java/io/github/meowhalla/data/WeaponType.java
@@ -0,0 +1,33 @@
+package io.github.meowhalla.data;
+
+import com.badlogic.gdx.math.Vector2;
+import io.github.meowhalla.classes.WeaponContext;
+
+public enum WeaponType {
+    MAGIC_CRUSHER(
+        new WeaponContext(
+        "Magic Crusher",
+            20,
+            "Magic Orb.png",
+            0.5f,
+            new Vector2(350, 0),
+            20
+        )),
+
+    LIGHT_BLESSING(
+        new WeaponContext(
+            "Light Blessing",
+            13,
+            "Pure Bolt 2.png",
+            0.3f,
+            new Vector2(700, 0),
+            8
+        ));
+
+    public final WeaponContext data;
+
+    WeaponType(WeaponContext data) {
+        this.data = data;
+    }
+}
+
